46986ee779c5846caba8610f34d58aba4ebd8a08
Darren Felton <darren_felton@hotmail.com>
2019-03-15 20:01:50 -0500

    WIP - start of the simple trade repeater

---
+++ src/Command/Command/SimpleTrader/Buyer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Kobens\Exchange\Command\Command\SimpleTrader;
+
+use Kobens\Core\Command\Traits\Traits;
+use Kobens\Exchange\Trader\SimpleRepeater;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Kobens\Exchange\Exchange\Mapper;
+use Zend\Db\ResultSet\ResultSet;
+
+class Buyer extends Command
+{
+    use Traits;
+
+    /**
+     * @var SimpleRepeater
+     */
+    protected $simpleRepeater;
+
+    protected $hasReportedUpToDate = false;
+
+    protected function configure()
+    {
+        $this->setName('exchange:trader:simple-repeater:buyer');
+        $this->setDescription('Performs buys for the simple repeater trader.');
+    }
+
+    protected function initialize($input, $output)
+    {
+        $this->simpleRepeater = new SimpleRepeater();
+        $this->exchangeMapper = new Mapper();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->loop($output);
+    }
+
+    protected function loop(OutputInterface $output)
+    {
+        $resultSet = $this->simpleRepeater->getOrdersToBuy();
+        if ($resultSet->count() === 0) {
+            $this->clearTerminal($output);
+            if ($this->hasReportedUpToDate === false) {
+                $output->write('All active buy orders up to date.');
+                $this->hasReportedUpToDate = true;
+            }
+        } else {
+            $this->hasReportedUpToDate = false;
+            $this->clearTerminal($output);
+            $output->write('Detected buy orders ready to place.');
+            $this->placeOrders($resultSet, $output);
+        }
+
+        \sleep(1);
+        $output->write('.');
+        $this->loop($output);
+    }
+
+    protected function placeOrders(ResultSet $resultSet, OutputInterface $output)
+    {
+        for ($i=0;$i<=5;$i++) {
+            $output->write('.');
+        }
+    }
+
+}
---
+++ src/Exchange/Mapper.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Kobens\Exchange\Exchange;
+
+use Kobens\Exchange\Exception\Exception;
+use Kobens\Exchange\ExchangeInterface;
+
+class Mapper
+{
+    /**
+     * @var string
+     */
+    protected static $mappings = [];
+
+    public function __construct(array $mappings = null)
+    {
+        if ($mappings !== [] && static::$mappings !== []) {
+            throw new \LogicException(\sprintf(
+                '"%s" cannot be reinstantiated in the same thread with additional mappings.',
+                static::class
+            ));
+        }
+        $this->addMappings($mappings);
+    }
+
+    protected function addMappings(array $mappings) : void
+    {
+        foreach ($this->validateMappings($mappings) as $map) {
+            static::$mappings[$map['key']] = $map['className'];
+        }
+    }
+
+    protected function validateMappings(array $mappings) : \Generator
+    {
+        foreach ($mappings as $key => $className) {
+            if (!\class_exists($className)) {
+                throw new Exception(\sprintf(
+                    'Exchange map class "%s" not found for key "%s".',
+                    $className,
+                    $key
+                ));
+            }
+
+            $implements = \class_implements($className);
+            if (!isset($implements[ExchangeInterface::class])) {
+                throw new Exception(\sprintf(
+                    '"%s" only accepts interfaces of "%s". "%s" is lacking this requirement.',
+                    static::class,
+                    ExchangeInterface::class,
+                    $className
+                ));
+            }
+
+            yield [
+                'key' => $key,
+                'className' => $className
+            ];
+        }
+    }
+
+    protected function getClassMap(string $key) : string
+    {
+
+    }
+}
